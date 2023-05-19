6a1ac47ba3ebe79e7d2b1b17f527dc4a1e88043f
bearwng10 <96107954+bearwng10@users.noreply.github.com>
2023-05-19 13:14:37 -0500

    Update index.js
    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
+console.log(companyName);
+console.log(mostProfitableNeighborhood);
+console.log(companyCeo);
