bf71d3f11756914647fc7c9692110bc38ab8424e
bearwng10 <96107954+bearwng10@users.noreply.github.com>
2023-06-06 11:16:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const companyName = 'Scuber';
-let mostProfitableNeighborhood = 'Chelsea';
-let companyCeo = 'Susan Smith';
+// index.js
 
-console.log(companyName);
-console.log(mostProfitableNeighborhood);
-console.log(companyCeo);
+// Define the currentUser variable with a string value
+const currentUser = 'John Doe';
+
+// Define the welcomeMessage variable using the currentUser variable
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excitedWelcomeMessage variable using the currentUser variable
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define the shortGreeting variable using the currentUser variable
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
